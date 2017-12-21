48d59eb70d37097ea7c1dedda4b0a714f0b5b007
Shawn Tolidano <shawn@tolidano.com>
2017-12-21 00:57:37 -0500

    MTA real time feed updates

--- data/parse_mta_feed_gtfs.py
+++ data/parse_mta_feed_gtfs.py
@@ -6,23 +6,77 @@ from google.transit import gtfs_realtime_pb2
 
 def main(file):
     """
-
+    Parse the various GTFS status entities.
     """
+    info = {'trip_updates': [], 'vehicles': [], 'alerts': []}
     try:
         with open(file, 'rb') as gtfs:
             data = gtfs.read()
             feed = gtfs_realtime_pb2.FeedMessage()
             feed.ParseFromString(data)
+            # https://developers.google.com/transit/gtfs-realtime/examples/trip-updates-full
             for entity in feed.entity:
                 if entity.HasField('trip_update'):
-                    print('----TRIP_UPDATE----')
                     print(entity)
+                    info['trip_updates'].append({
+                        'start': '1',
+                    })
                 elif entity.HasField('vehicle'):
-                    print('----VEHICLE----')
-                    print(entity)
+                    info['vehicles'].append({
+                        'id': entity.vehicle.vehicle.id,
+                        'stop_id': entity.vehicle.stop_id,
+                        'timestamp': entity.vehicle.timestamp,
+                        'latitude': entity.vehicle.position.latitude,
+                        'longitude': entity.vehicle.position.longitude,
+                        'bearing': entity.vehicle.position.bearing,
+                        'trip_id': entity.vehicle.trip.trip_id,
+                        'start_date': entity.vehicle.trip.start_date,
+                        'route_id': entity.vehicle.trip.route_id,
+                        'direction_id': entity.vehicle.trip.direction_id,
+                    })
                 elif entity.HasField('alert'):
-                    print('----ALERT----')
-                    print(entity)
+                    times = []
+                    active_periods = len(entity.alert.active_period)
+                    if active_periods:
+                        for period in entity.alert.active_period:
+                            time = {
+                                'start': period.start,
+                            }
+                            if period.HasField('end'):
+                                time['end'] = period.end
+                            times.append(time)
+                    entities = []
+                    informed_entities = len(entity.alert.informed_entity)
+                    if informed_entities:
+                        for informed in entity.alert.informed_entity:
+                            entities.append({
+                                'agency_id': informed.agency_id,
+                                'route_id': informed.trip.route_id,
+                                'direction_id': informed.trip.direction_id,
+                            })
+                    cause = 'NONE'
+                    if entity.alert.HasField('cause'):
+                        cause = entity.alert.cause
+                    effect = 'NONE'
+                    if entity.alert.HasField('effect'):
+                        effect = entity.alert.effect
+                    header = 'NONE'
+                    if entity.alert.HasField('header_text'):
+                        header = entity.alert.header_text.translation[0].text
+                    description = 'NONE'
+                    if entity.alert.HasField('description_text'):
+                        description = entity.alert.description_text.translation[0].text
+                    info['alerts'].append({
+                        'times': times,
+                        'entities': entities,
+                        'cause': cause,
+                        'effect': effect,
+                        'header': header,
+                        'description': description,
+                    })
+        print(info['vehicles'])
+        print(info['alerts'])
+        print(info['trip_updates'])
     except IOError:
         print('{} not found.'.format(file))
 
